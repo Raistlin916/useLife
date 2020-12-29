9e508078f22be575b0f782c503cd64b9cd7dff04
gaokai <raistlin916@gmail.com>
2020-12-29 20:49:11 +0800

    rename default

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,6 @@ export * from './useRequest'
 export * from './usePaginationRequest'
 export * from './useResource'
 
+export { default as useRequest } from './usePaginationRequest'
+export { default as usePaginationRequest } from './usePaginationRequest'
+
